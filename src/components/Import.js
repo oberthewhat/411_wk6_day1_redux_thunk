cac42f46309890211e0b931c409eb0eda6974521
John Obertubbesing <oberthewhat@gmail.com>
2020-02-24 20:39:05 -0600

    think i thunk

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Button, Table, TableHead, TableBody, TableRow, TableCell, Container } from "@material-ui/core"
 
 const Import = (props) => {
-    // fill out this component
-
     return (
-        <p>Import Component</p>
+        <div>
+          <p>Import Component</p>
+          <Button variant="contained" color="primary" onClick={props.fetchMakes}>
+          Import
+          </Button>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell align="right">ID</TableCell>
+                <TableCell align="right">Make&nbsp;</TableCell>
+                <TableCell align="right">Actions&nbsp;</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            {props.makes.map(row => (
+            <TableRow key={row.MakeId}>
+              <TableCell component="th" scope="row">
+                {row.MakeId}
+              </TableCell>
+              <TableCell align="right">{row.MakeId}</TableCell>
+              <TableCell align="right">{row.MakeName}</TableCell>
+              <TableCell align="right">{row.Actions}</TableCell>
+            </TableRow>
+          ))}
+            </TableBody>
+          </Table>
+        </div>
     )
 }
 
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -13,3 +13,16 @@ export const removeCar = (index) => {
         value: index
     }
 }
+export const fetchMakes = () => {
+    return (dispatch) => {
+        fetch("https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json")
+            .then(res => res.json())
+            .then(response => {
+                const action = {
+                    type: 'FETCH_MAKES',
+                    value: response.Results
+                }
+                dispatch(action)
+            })
+    }
+}
